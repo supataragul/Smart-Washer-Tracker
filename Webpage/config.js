1ce2437e44aa7368e5c56bc84ff2069466cf5866
supataragul <44852041+supataragul@users.noreply.github.com>
2019-03-12 02:42:45 -0400

    Update config.js

--- Webpage/config.js
+++ Webpage/config.js
@@ -15,7 +15,7 @@ module.exports = {
   projectId: 'swt-mu',
   keyFilename: './key.json',
   email: 'smartwashertracker@gmail.com',
-  emailPassword: 'P@$$word_washer',
+  emailPassword: '...@......_washer',
   cookieSecret: 'D738BAC55117DD662673C85A94854',//256-bit WEP Keys
   oauth2: {
     clientId: '301016823163-0o66dnc9m5o7octpa5ims3p5cborifsr.apps.googleusercontent.com',
